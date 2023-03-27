2df765560b77eb6a6316a28f4088fb09824de6ab
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-27 13:21:33 +0900

    Refactor Viewcontrollers

--- RickAndMorty/Controllers/Core/RMTabbarController.swift
+++ RickAndMorty/Controllers/Core/RMTabbarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RMTabbarController: UITabBarController {
+final class RMTabbarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
