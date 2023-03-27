64f261296ee0ede997eac5803664bb3a6224305d
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-27 11:57:07 +0900

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by 山本響 on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
